f3a893a2eb9b8e4d94c3069f4df4e3d21a87a414
manuelechavarria <maechavarriaor@gmail.com>
2026-07-26 11:35:48 -0500

    Creditos

--- src/config/global.js
+++ src/config/global.js
@@ -376,6 +376,11 @@ export default {
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
         {
+          nombre: 'Manuel Felipe Echavarria Orozco',
+          cargo: 'Desarrollador <em>full stack</em>',
+          centro: 'Centro de Comercio y Servicios - Regional Tolima',
+        },
+        {
           nombre: 'Gilberto Junior Rodríguez Rodríguez',
           cargo: 'Animador y productor audiovisual',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
